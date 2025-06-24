5ea4dd8e3370d1a76b7e21a8ebc0d6d68dea5edd
rijo7 <rijov929@gmail.com>
2025-06-24 21:39:38 +0530

    Update settings

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
