78e409de76d27176ab7576a537d58d237fede275
rijo7 <rijov929@gmail.com>
2025-06-25 15:39:03 +0530

    Fix ALLOWED_HOSTS environment variable parsing

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost', '127.0.0.1').split(',')
+ALLOWED_HOSTS = ['ALLOWED_HOSTS', 'localhost', '127.0.0.1']
 
 SECURE_SSL_REDIRECT = os.getenv('RENDER', False) == 'true'
 # Application definition
